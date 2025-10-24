d1e16e50de5a18642bbc54c2a28a677088baee53
KasunXploit SL <lakshank228@gmail.com>
2025-10-24 15:56:16 +0530

    Add Mini-Max Sum algorithm implementation
    Implement miniMaxSum function to calculate min and max sums of an array.

---
+++ algorithms/warmup/Mini-MaxSum.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        long min=0,max=0,sum=0;
+        min=arr.get(0);
+        max=min;
+        sum=min;
+        int len=arr.size();
+        for(int i =1;i<len;i++){
+            sum+=arr.get(i);
+            if(arr.get(i)<min){
+                min=arr.get(i);
+            }
+            if (arr.get(i)>max){
+                max=arr.get(i);
+            }
+        }
+        System.out.print((sum-max)+" "+(sum-min));
+        }
+         
+    // Write your code here
+
+    }
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
