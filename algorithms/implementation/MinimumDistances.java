6449f7d557a867e06b7bdbebef11932fca7934d5
KasunXploit SL <lakshank228@gmail.com>
2025-11-05 19:14:33 +0530

    Create MinimumDistances.java

---
+++ algorithms/implementation/MinimumDistances.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+        int distance = 0;
+        int minimumDistance=Integer.MAX_VALUE;
+        for(int i = 0 ; i < a.size() ; i++){
+            int current = a.get(i);
+            for (int j=i+1 ; j < a.size() ; j++){
+                if (current == a.get(j)){
+                    distance = j-i;
+                    
+                    if(distance < minimumDistance){
+                        minimumDistance=distance;
+                    }
+                    break;
+                }
+               
+            }
+            
+        } return (minimumDistance == Integer.MAX_VALUE) ? -1 : minimumDistance;
+
+   
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] aTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aTemp[i]);
+            a.add(aItem);
+        }
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
